fd8081ddd6f1b157cc312b205051f17f68c7d14e
muflehahmad <muflehahmad96@gmail.com>
2022-10-17 01:55:46 +0100

    Add files via upload

---
+++ deletedisks.sh
@@ -0,0 +1,17 @@
+# Set deleteUnattachedDisks=1 if you want to delete unattached Managed Disks
+# Set deleteUnattachedDisks=0 if you want to see the Id of the unattached Managed Disks
+deleteUnattachedDisks=1
+unattachedDiskIds=$(az disk list --query '[?managedBy==`null`].[id]' -o tsv)
+for id in ${unattachedDiskIds[@]}
+do
+    if (( $deleteUnattachedDisks == 1 ))
+    then
+
+        echo "Deleting unattached Managed Disk with Id: "$id
+        az disk delete --ids $id --yes
+        echo "Deleted unattached Managed Disk with Id: "$id
+
+    else
+        echo $id
+    fi
+done
